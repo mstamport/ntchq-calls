f471c136d16a6415399ad737440e44c86c697260
mstamport <30299045+mstamport@users.noreply.github.com>
2018-04-24 08:45:49 -0500

    Made requested CSV changes

--- server.go
+++ server.go
@@ -40,25 +40,23 @@ func getReportDataSetRawData(w http.ResponseWriter, r *http.Request){
 	 writer := csv.NewWriter(b) 
 
   var record []string
+    record = append(record, "Headquarter_ID")
+    record = append(record, "CallHeadquarterID")
     record = append(record, "Zone")
     record = append(record, "Region")
     record = append(record, "Territory_Owner")
     record = append(record, "Type")
-    record = append(record, "Territory")
-    record = append(record, "Headquarter_ID")
-    record = append(record, "CallHeadquarterID")
     record = append(record, "FirstCall")
     record = append(record, "LastCall")
+    record = append(record, "Headquarter")
     record = append(record, "HeadquarterStatus")
     record = append(record, "HeadquarterNumber")
     record = append(record, "Address")
     record = append(record, "City")
     record = append(record, "State")
     record = append(record, "Zip")
-    record = append(record, "Headquarter")
     record = append(record, "PrimaryWholesaler")
     record = append(record, "PWCustomerNumber")
-    record = append(record, "HeadquarterID")
     record = append(record, "Frequency")
     record = append(record, "Year")
     record = append(record, "Week")
@@ -68,6 +66,7 @@ func getReportDataSetRawData(w http.ResponseWriter, r *http.Request){
     record = append(record, "CallType")
     record = append(record, "LastDistributionCall")
     record = append(record, "TerritoryCoverage")
+    record = append(record, "Notes")
     record = append(record, "Contact")
     record = append(record, "Stokers Cans") 
     record = append(record, "Stokers Tubs")
@@ -106,36 +105,25 @@ func getReportDataSetRawData(w http.ResponseWriter, r *http.Request){
       resultReflectionValue := reflect.ValueOf(elementRawData)
       resultInterface := resultReflectionValue.Interface()
       resultMap := resultInterface.(map[string]interface{})
-      /*
-      if(i == 0){
-          keys := reflect.ValueOf(resultMap).MapKeys()
-          var record []string
-          for j := 0; j < len(keys); j++ {
-            record = append(record, keys[j].String())
-         }
-         writer.Write(record)
-      }*/
 
      var record []string
+      record = append(record, resultMap["Headquarter_ID"].(string))
+      record = append(record, resultMap["CallHeadquarterID"].(string))
       record = append(record, resultMap["Zone"].(string))
       record = append(record, resultMap["Region"].(string))
       record = append(record, resultMap["Territory_Owner"].(string))
       record = append(record, resultMap["Type"].(string))
-      record = append(record, resultMap["Territory"].(string))
-      record = append(record, resultMap["Headquarter_ID"].(string))
-      record = append(record, resultMap["CallHeadquarterID"].(string))
       record = append(record, resultMap["FirstCall"].(string))
       record = append(record, resultMap["LastCall"].(string))
+      record = append(record, resultMap["Headquarter"].(string))
       record = append(record, resultMap["HeadquarterStatus"].(string))
       record = append(record, resultMap["HeadquarterNumber"].(string))
       record = append(record, resultMap["Address"].(string))
       record = append(record, resultMap["City"].(string))
       record = append(record, resultMap["State"].(string))
       record = append(record, resultMap["Zip"].(string))
-      record = append(record, resultMap["Headquarter"].(string))
       record = append(record, resultMap["PrimaryWholesaler"].(string))
       record = append(record, resultMap["PWCustomerNumber"].(string))
-      record = append(record, resultMap["HeadquarterID"].(string))
       record = append(record, resultMap["Frequency"].(string))
       record = append(record, resultMap["Year"].(string))
       record = append(record, resultMap["Week"].(string))
@@ -145,6 +133,7 @@ func getReportDataSetRawData(w http.ResponseWriter, r *http.Request){
       record = append(record, resultMap["CallType"].(string))
       record = append(record, resultMap["LastDistributionCall"].(string))
       record = append(record, resultMap["TerritoryCoverage"].(string))
+      record = append(record, resultMap["Notes"].(string))
       record = append(record, resultMap["Contact"].(string))
       record = append(record, resultMap["Stokers Cans"].(string)) 
       record = append(record, resultMap["Stokers Tubs"].(string))
@@ -182,11 +171,196 @@ func getReportDataSetRawData(w http.ResponseWriter, r *http.Request){
     w.Header().Set("Content-Type", "text/csv") // setting the content type header to text/csv
 
 	 w.Header().Set("Content-Type", "text/csv")
+	 w.Header().Set("Content-Disposition", "attachment;filename=Sheet1.csv")
+	 w.Write(b.Bytes())
+}
+
+
+func getReportCSVDataSetRawData(w http.ResponseWriter, r *http.Request){
+    fmt.Println("URL: " + r.URL.Host + r.URL.Path)
+    data := getReportCSVDataResultSet(w, r)
+    columndata := getReportRawDataColumnSet(w, r)
+    fmt.Println("Endpoint Hit: getReportCSVDataResultSet")
+
+    b := &bytes.Buffer{} // creates IO Writer  
+	 writer := csv.NewWriter(b) 
+
+   var record []string
+    record = append(record, "HQ_HQID")
+    record = append(record, "CallHQ_ID")
+    record = append(record, "Zone")
+    record = append(record, "Region")
+    record = append(record, "Territory_Owner")
+    record = append(record, "Type")
+    record = append(record, "FirstCall")
+    record = append(record, "LastCall")
+    record = append(record, "HQ_Name")
+    record = append(record, "HQStatus")
+    record = append(record, "HQNumber")
+    record = append(record, "Address")
+    record = append(record, "City")
+    record = append(record, "State")
+    record = append(record, "Zip")
+    record = append(record, "PrimaryWholesaler")
+    record = append(record, "PrimaryWholesalerCustNumber")
+    record = append(record, "Frequency")
+    record = append(record, "Year")
+    record = append(record, "Week")
+    record = append(record, "Quarter")
+    record = append(record, "Assigned")
+    record = append(record, "CallDate")
+    record = append(record, "CallType")
+    record = append(record, "LastDistributionCall")
+    record = append(record, "Territory Coverage")
+    record = append(record, "Notes")
+    record = append(record, "Contact")
+    for x := 0; x < len(columndata.Rows); x++ {
+       var elementColumnData = columndata.Rows[x]
+       resultReflectionColValue := reflect.ValueOf(elementColumnData )
+       resultColInterface := resultReflectionColValue.Interface()
+       resultColMap := resultColInterface.(map[string]interface{}) 
+       record = append(record, resultColMap["ColumnName"].(string)) 
+    } 
+ 
+    writer.Write(record)
+
+    fmt.Println("Header created")
+
+    for i := 0; i < len(data.Rows); i++ {
+      var elementRawData = data.Rows[i]
+      resultReflectionValue := reflect.ValueOf(elementRawData)
+      resultInterface := resultReflectionValue.Interface()
+      resultMap := resultInterface.(map[string]interface{})
+
+     var record []string
+      record = append(record, resultMap["Headquarter_ID"].(string))
+      record = append(record, resultMap["CallHeadquarterID"].(string))
+      record = append(record, resultMap["Zone"].(string))
+      record = append(record, resultMap["Region"].(string))
+      record = append(record, resultMap["Territory_Owner"].(string))
+      record = append(record, resultMap["Type"].(string))
+      record = append(record, resultMap["FirstCall"].(string))
+      record = append(record, resultMap["LastCall"].(string))
+      record = append(record, resultMap["Headquarter"].(string))
+      record = append(record, resultMap["HeadquarterStatus"].(string))
+      record = append(record, resultMap["HeadquarterNumber"].(string))
+      record = append(record, resultMap["Address"].(string))
+      record = append(record, resultMap["City"].(string))
+      record = append(record, resultMap["State"].(string))
+      record = append(record, resultMap["Zip"].(string))
+      record = append(record, resultMap["PrimaryWholesaler"].(string))
+      record = append(record, resultMap["PWCustomerNumber"].(string))
+      record = append(record, resultMap["Frequency"].(string))
+      record = append(record, resultMap["Year"].(string))
+      record = append(record, resultMap["Week"].(string))
+      record = append(record, resultMap["Quarter"].(string))
+      record = append(record, resultMap["Assigned"].(string))
+      record = append(record, resultMap["Call"].(string))
+      record = append(record, resultMap["CallType"].(string))
+      record = append(record, resultMap["LastDistributionCall"].(string))
+      record = append(record, resultMap["TerritoryCoverage"].(string))
+      record = append(record, resultMap["Notes"].(string))
+      record = append(record, resultMap["Contact"].(string))
+      for x := 0; x < len(columndata.Rows); x++ {
+         var elementColumnData = columndata.Rows[x]
+         resultReflectionColValue := reflect.ValueOf(elementColumnData )
+         resultColInterface := resultReflectionColValue.Interface()
+         resultColMap := resultColInterface.(map[string]interface{}) 
+         record = append(record, resultMap[resultColMap["ItemID"].(string)].(string))
+       } 
+
+ 
+      writer.Write(record)
+    }
+
+    writer.Flush()
+
+    w.Header().Set("Content-Type", "text/csv") // setting the content type header to text/csv
+
+	 w.Header().Set("Content-Type", "text/csv")
 	 w.Header().Set("Content-Disposition", "attachment;filename=HQCalls.csv")
 	 w.Write(b.Bytes())
 }
 
-func getReportRawDataResultSet(w http.ResponseWriter, r *http.Request) RawDataSearchResults{
+
+func getReportCSVDataResultSet(w http.ResponseWriter, r *http.Request) CSVDataSearchResults {
+    r.ParseForm()
+    requestedPageNumber := r.FormValue("page")
+    rowsCount :=  r.FormValue("rows")
+    columnToSort :=  r.FormValue("sidx")
+    sortDirection :=  r.FormValue("sord")
+    filters :=  r.FormValue("filters")
+    
+    fmt.Println("RequestedPageNumber : " + requestedPageNumber )
+    fmt.Println("RowsCount : " + rowsCount )
+    fmt.Println("ColumnToSort : " + columnToSort )
+    fmt.Println("SortDirection : " + sortDirection )
+    fmt.Println("Filters : " + filters )
+
+    dec := json.NewDecoder(strings.NewReader(filters))
+    var f Filters
+    var filterBuffer bytes.Buffer
+
+    for {
+		if err := dec.Decode(&f); err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+   for index, element := range f.Rules {
+
+     if  (element.Data != ""){
+       if(index == 0){
+         filterBuffer.WriteString(" where ")
+       }
+       if(index > 0){
+         filterBuffer.WriteString(" " + f.GroupOp + " ")
+       }
+       filterBuffer.WriteString(" [" + element.Field + "] ")
+       switch element.Op{
+          case "bw": // Begins With
+            filterBuffer.WriteString( " like '" + element.Data + "%'")
+         case "bn": // Does not begin with
+            filterBuffer.WriteString( " not like '" + element.Data + "%'")
+          case "eq": // Equal
+            filterBuffer.WriteString( " = '" + element.Data + "'")
+          case "ne": // Not Equal
+            filterBuffer.WriteString( " != '" + element.Data + "'")       
+          case "lt": // Less than
+            filterBuffer.WriteString( " < '" + element.Data + "'")       
+          case "le": // Less than or equal
+            filterBuffer.WriteString( " <= '" + element.Data + "'")       
+          case "gt": // Greater than
+            filterBuffer.WriteString( " > '" + element.Data + "'")       
+          case "ge": // Greater than or equal
+            filterBuffer.WriteString( " >= '" + element.Data + "'")       
+          case "ew": // Ends with
+            filterBuffer.WriteString( " like '%" + element.Data + "'")       
+          case "en": // Does not ends with
+            filterBuffer.WriteString( " not like '%" + element.Data + "'")       
+          case "nu": // is null
+            filterBuffer.WriteString( " is null ")       
+          case "nn": // is not null
+            filterBuffer.WriteString( " is not null ")       
+          case "in": // in
+            filterBuffer.WriteString( " in (" + element.Data + ")")       
+          case "ni": // not in
+            filterBuffer.WriteString( " not in (" + element.Data + ")")       
+          case "nc": // Does not contain
+            filterBuffer.WriteString( " not like '%" + element.Data + "%'")       
+          default: // contains
+            filterBuffer.WriteString( " like '%" + element.Data + "%'")       
+       }
+       fmt.Println("SQL Filters : " + filterBuffer.String() )
+     }
+   }
+
+    return dalGetCSVDataSet(requestedPageNumber, rowsCount, columnToSort, sortDirection, filterBuffer.String());
+}
+
+func getReportRawDataResultSet(w http.ResponseWriter, r *http.Request) CSVDataSearchResults{
     r.ParseForm()
     requestedPageNumber := r.FormValue("page")
     rowsCount :=  r.FormValue("rows")
@@ -218,9 +392,9 @@ func getReportRawDataResultSet(w http.ResponseWriter, r *http.Request) RawDataSe
          filterBuffer.WriteString(" where ")
        }
        if(index > 0){
-         filterBuffer.WriteString(" and ")
+         filterBuffer.WriteString(" " + f.GroupOp + " ")
        }
-       filterBuffer.WriteString(element.Field)
+       filterBuffer.WriteString(" [" + element.Field + "] ")
        switch element.Op{
           case "bw": // Begins With
             filterBuffer.WriteString( " like '" + element.Data + "%'")
@@ -261,12 +435,88 @@ func getReportRawDataResultSet(w http.ResponseWriter, r *http.Request) RawDataSe
     return dalGetReportDataSet(requestedPageNumber, rowsCount, columnToSort, sortDirection, filterBuffer.String());
 }
 
+func getReportRawDataColumnSet(w http.ResponseWriter, r *http.Request) RawDataColumnResults {
+    r.ParseForm()
+    requestedPageNumber := r.FormValue("page")
+    rowsCount :=  r.FormValue("rows")
+    columnToSort :=  r.FormValue("sidx")
+    sortDirection :=  r.FormValue("sord")
+    filters :=  r.FormValue("filters")
+    
+    fmt.Println("RequestedPageNumber : " + requestedPageNumber )
+    fmt.Println("RowsCount : " + rowsCount )
+    fmt.Println("ColumnToSort : " + columnToSort )
+    fmt.Println("SortDirection : " + sortDirection )
+    fmt.Println("Filters : " + filters )
+
+    dec := json.NewDecoder(strings.NewReader(filters))
+    var f Filters
+    var filterBuffer bytes.Buffer
+
+    for {
+		if err := dec.Decode(&f); err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+   for index, element := range f.Rules {
+
+       if(index == 0){
+         filterBuffer.WriteString(" where ")
+       }
+       if(index > 0){
+         filterBuffer.WriteString(" " + f.GroupOp + " ")
+       }
+       filterBuffer.WriteString(" [" + element.Field + "] ")
+       switch element.Op{
+          case "bw": // Begins With
+            filterBuffer.WriteString( " like '" + element.Data + "%'")
+         case "bn": // Does not begin with
+            filterBuffer.WriteString( " not like '" + element.Data + "%'")
+          case "eq": // Equal
+            filterBuffer.WriteString( " = '" + element.Data + "'")
+          case "ne": // Not Equal
+            filterBuffer.WriteString( " != '" + element.Data + "'")       
+          case "lt": // Less than
+            filterBuffer.WriteString( " < '" + element.Data + "'")       
+          case "le": // Less than or equal
+            filterBuffer.WriteString( " <= '" + element.Data + "'")       
+          case "gt": // Greater than
+            filterBuffer.WriteString( " > '" + element.Data + "'")       
+          case "ge": // Greater than or equal
+            filterBuffer.WriteString( " >= '" + element.Data + "'")       
+          case "ew": // Ends with
+            filterBuffer.WriteString( " like '%" + element.Data + "'")       
+          case "en": // Does not ends with
+            filterBuffer.WriteString( " not like '%" + element.Data + "'")       
+          case "nu": // is null
+            filterBuffer.WriteString( " is null ")       
+          case "nn": // is not null
+            filterBuffer.WriteString( " is not null ")       
+          case "in": // in
+            filterBuffer.WriteString( " in (" + element.Data + ")")       
+          case "ni": // not in
+            filterBuffer.WriteString( " not in (" + element.Data + ")")       
+          case "nc": // Does not contain
+            filterBuffer.WriteString( " not like '%" + element.Data + "%'")       
+          default: // contains
+            filterBuffer.WriteString( " like '%" + element.Data + "%'")       
+       }
+       fmt.Println("SQL Filters : " + filterBuffer.String() )
+   }
+
+    return dalGetRawColumnsSet(requestedPageNumber, rowsCount, columnToSort, sortDirection, filterBuffer.String());
+
+}
+
 func handleRequests(serverAddress string) {
 
     myRouter := mux.NewRouter().StrictSlash(true).UseEncodedPath()
     myRouter.HandleFunc("/", homePage)
     myRouter.HandleFunc("/reportdataset", getReportDataSet).Methods("GET", "POST")
-    myRouter.HandleFunc("/reportdatasetrawdata", getReportDataSetRawData).Methods("GET", "POST")
+    myRouter.HandleFunc("/reportdatasetrawdata", getReportCSVDataSetRawData).Methods("GET", "POST")
     myRouter.HandleFunc("/hqCallExcelReport", HQCallExcelReport).Methods("GET", "POST")
 
     http.Handle("/", &MyServer{myRouter})
